df64d46ef63b06c642d6537c050dcdd40d2039dd
senthiltechspot <senthilm784@gmail.com>
2023-06-05 21:52:41 +0530

    Backend ixed by render

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import io from "socket.io-client";
 
-const socket = io("https://realtime-chat-app-senthiltechspot.vercel.app");
+const socket = io("https://chat-app-api-senthiltechspot.onrender.com/");
 
 function App() {
   const [message, setMessage] = useState("");
