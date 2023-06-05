1853a4d247766ee1a9005c1a9de25576e96ebd7f
senthiltechspot <senthilm784@gmail.com>
2023-06-05 21:20:32 +0530

    Ready to deploy

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const server = http.createServer(app);
 const messages = [];
 const io = socketIO(server, {
   cors: {
-    origin: "",
+    origin: "*",
     methods: ["GET", "POST"],
     credentials: true,
   },
